27d2fdf052c0caa0a59030345aa367646785ead6
anastasia <legkova.anastasia127@gmail.com>
2023-07-11 09:57:25 +0300

    dialogs

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,6 @@
+import s from './Dialogs.module.css'
+export const Dialogs = () => {
+    return(
+        <div>Dialogs</div>
+    )
+}
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,9 @@
 import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 export const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
-            <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+            <img className={s.img} src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
         </div>
         <div>ava + description</div>
         <MyPosts/>

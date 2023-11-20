a4440962e9e4ca681f756e81d80e968370ce28f6
anastasia <legkova.anastasia127@gmail.com>
2023-11-20 11:47:01 +0300

    effect-hook

--- src/components/Profile/MyPosts/PropfileInfo/ProfileStatus.tsx
+++ src/components/Profile/MyPosts/PropfileInfo/ProfileStatus.tsx
@@ -1,10 +1,14 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {MapDispatchToPropsType, MapStateToPropsType} from "../../ProfileContainer";
 
 export const ProfileStatus = (props: MapStateToPropsType & MapDispatchToPropsType) => {
 
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    }, [props.status])
     const activateEditMode = () => {
         setEditMode(true)
     }
